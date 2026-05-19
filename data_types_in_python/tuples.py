602c757ba047fc9b589a6c76858e8125e61f7c86
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-05-19 11:01:56 +0530

    Add tuples learning file

---
+++ data_types_in_python/tuples.py
@@ -0,0 +1,51 @@
+# Tuples ------> ()
+
+names = ("xyz", "abc", "def")
+(a,b,c) = names
+print(f"names are : {a}, {b}, {c}")
+
+abcd, efgh = 1,2
+print(f"ratio of abcd is  : {abcd} and efgh is {efgh}")
+abcd, efgh = efgh, abcd
+print(f"ratio of abcd is  : {abcd} and efgh is {efgh}")
+print(f"is xyz available in name ? {"xyz" in names}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
